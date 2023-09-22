d48abaa8403b82c0d845b9fa4dd3c6c10314d368
Sathya Padmanabhan <72115986+TheSacredJump@users.noreply.github.com>
2023-09-22 15:32:23 -0500

    Add files via upload

---
+++ llm2.py
@@ -0,0 +1,25 @@
+import openai
+import gradio
+from gradio_client import Client
+
+#Get the OpenAI API Key
+openai.api_key = open('LLM Project/OpenAI_API_Key', 'r').read()
+
+messages=[]
+
+#Create a custom chatgpt model
+def custom_chatgpt(query):
+    messages.append({"role": "user", "content": query})
+    response = openai.ChatCompletion.create(
+        model = "gpt-3.5-turbo",
+        messages = messages
+    )
+    chatgpt_reply = response["choices"][0]["message"]["content"]
+    messages.append({"role": "user", "content": chatgpt_reply})
+    return chatgpt_reply
+
+
+
+demo = gradio.Interface(fn=custom_chatgpt, inputs="text", outputs="text", title="Garuda GPT")
+
+demo.launch()
